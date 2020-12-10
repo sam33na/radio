df268c952a75835c6ccbae1ac853147b63bbf048
sam33na <saminashrestha98@gmail.com>
2020-12-10 13:11:25 +0545

    grid done

---
+++ app/src/main/java/com/wdg/widgets/gridLayout.kt
@@ -0,0 +1,11 @@
+package com.wdg.widgets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gridLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gridlayout)
+    }
+}
